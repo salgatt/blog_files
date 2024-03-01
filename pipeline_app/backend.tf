4f69e66cb9f5ac3babc013c6f518f0e8f977f3ee
Scott <salgatt@turtleshell.net>
2024-02-29 20:24:16 -0500

    adding the backend

--- pipeline_app/backend.tf
+++ pipeline_app/backend.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   backend "s3" {
-#     # Replace this with your bucket name!
-#     bucket = "TF_BACKEND_S3_BUCKET"
-#     key    = "global/s3/terraform.tfstate"
-#     region = "AWS_REGION"
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "TF_BACKEND_S3_BUCKET"
+    key    = "global/s3/terraform.tfstate"
+    region = "AWS_REGION"
 
-#     # Replace this with your DynamoDB table name!
-#     dynamodb_table = "TF_BACKEND_DYNAMO_TABLE"
-#     encrypt        = true
-#   }
-# }
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "TF_BACKEND_DYNAMO_TABLE"
+    encrypt        = true
+  }
+}
